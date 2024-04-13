2863923a44e18cbbdac9028a8baa1d1ab17693c6
DaniellGouvea <luizdaniel.04@gmail.com>
2024-04-13 16:39:16 -0300

    Criação de seleção na entrada do cadastro da senha e quando os campos estão vazios

---
+++ JavaProject/src/dao/Conexao.java
@@ -0,0 +1,18 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    
+    public Connection getConnection() throws SQLException{
+    
+        Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/bancojava", "root", "");
+        
+        return conexao;
+        
+    }
+    
+}
